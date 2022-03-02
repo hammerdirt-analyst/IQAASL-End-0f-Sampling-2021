be1f166ce54921e1171ba62b9de70538ae203e64
Roger Erismann <roger@hammerdirt.ch>
2022-03-02 02:51:22 +0100

    fd features, summary, component summary code totals

--- resources/feature_data.py
+++ resources/feature_data.py
@@ -1,10 +1,27 @@
-# defining the feature data object
+"""
+The feature data module caries out the operations included in ver1.0
+for the water body and survey area summaries. Additional features were
+added. Detailed component summaries are easy to generate, a report of the
+parameters is created on demand.
+
+The default attributes are based on the parameters of the end of sampling
+report in ver1.0. Once an instance of FeatureData  is instantiated
+attributes can be added in dict form. Using load_defaults(FeatureData())
+is the quickest way to get started.
+
+Changing <level>, <component> and <loi> attributes changes the geographical
+and the hierarchical context of the summary data. The <component> attribute
+must resolve to a subset of the <level> attribute. The FeatureData class
+gives the complete summary for the <level> attribute defined by the members
+of <loi> and a summary of all data above <level>
+"""
 import pandas as pd
 fd_var = {
-    "start": "2020-03-01",
-    "end": "2021-05-31",
+    "sdate": "2020-03-01",
+    "edate": "2021-05-31",
     "unit_label": "p/100m",
-    "a_fail_rate": 50
+    "a_fail_rate": 50,
+    "a_unit_length": 100
 }
 
 # set the different aggregation levels
@@ -31,7 +48,7 @@ data_source = {
   "codesource": "resources/codes_with_group_names_2015.csv",
   "dimsource": "resources/corrected_dims.csv",
   "notaggsource": "resources/checked_before_agg_sdata_eos_2020_21.csv",
-  "adatasource": "resources/checked_sdata_eos_2020_21.csv"
+  "fd": "resources/checked_sdata_eos_2020_21.csv"
 }
 
 column_rename = {
@@ -39,32 +56,55 @@ column_rename = {
     "% to recreation": "% recreation",
     "% to woods": "% woods",
     "% to buildings": "% buildings"
-}
+    }
+
+columns = [
+    "loc_date", "location",
+    "city", "water_name_slug",
+    "date", "quantity", "population"
+    ]
+
+explanatory_variables = [
+    "% buildings", "% recreation",
+    "% ag", "% woods", "streets km",
+    "intersects"
+    ]
+
+
+def load_defaults(x):
+    for atts in [fd_var, loc_var, data_source]:
+        x.add_attributes(atts)
+
 
 def add_attributes(self, attributes):
     for k, v in attributes.items():
-        setattr(self,key,value)
+        setattr(self, k, v)
 
 
-def feature_data(data, feature_level, these_features=[], **kwargs):
-    """Slices the data at the feature level for the features defined in these_features.
-    Defines the data that all analysis is based on.
+def feature_data(data, level: str = None, loi: list = [],
+                 slice_data: bool = True):
+    """Slices the data at the feature level for the features in loi.
 
     :param data: A pandas data frame of survey results
     :type data: object: pandas.core.frame.DataFrame
-    :param feature_level: The column name that contains
-        the groups for the desired aggregation level.
-    :type feature_level: str
-    :param these_features: An array of values that are contained in feature_level
-    :type feature_level: array
+    :param level: Column name that has will be aggregated on
+    :type level: str
+    :param loi: An array of values that are contained in feature_level
+    :type loi: array
+    :param slice_data: Indicate whether the data needs to be sliced
+    :type slice_data: bool
     :return: A sliced data frame with the date converted to datetime object
     :rtype: pandas.core.frame.DataFrame
     """
 
-    sliced_data = data[data[feature_level].isin(these_features)].copy()
-
+    if slice_data:
+        sliced_data = data[data[level].isin(loi)].copy()
+    else:
+        sliced_data = data.copy()
     # the loc_date is the unique indentifier for a survey
-    sliced_data["loc_date"] = list(zip(sliced_data.location.values, sliced_data["date"].values))
+    sliced_data["loc_date"] = list(zip(sliced_data.location.values,
+                                       sliced_data["date"].values)
+                                   )
 
     # ensure that the date is set to datetime
     sliced_data["date"] = pd.to_datetime(sliced_data["date"])
@@ -73,23 +113,283 @@ def feature_data(data, feature_level, these_features=[], **kwargs):
 
 
 class FeatureData:
-    def __init__(self, name: str = "Name on the title", level: str = "The aggregation level", component: str = "the smallest unit") -> None:
-            self.name = name
-            self.level = level
-            self.component = component
+    """The FeatureData class slices the data by user defined variables.
 
-    # use this method to add fd all the other variable attributes
-    def add_attributes(self, attributes):
-      for k, v in attributes.items():
-        setattr(self, k, v)
+    The class creates two different sets, one with all the data the other
+    with just the results from the loi. The summary statistics for the loi
+    and all the data are available as attributes of the class.
+    """
+
+    def __init__(self, name: str = None, level: str = None,
+                 component: str = None,
+                 min_att: list = ["level", "loi", "sdate", "edate"]
+                 ) -> None:
+        self.name = name
+        self.level = level
+        self.component = component
+        self.min_att = min_att
+
+    # use this method to add attributes to the class
+    def add_attributes(self, attributes: dict = {}):
+        for k, v in attributes.items():
+            setattr(self, k, v)
+
     # the value of class attributes
     def inst_attributes(self):
-        data = []
-        for a,v in self.__dict__.items():
-            data.append((a,v))
+        data = {k: type(v) for k, v in self.__dict__.items()}
         return data
 
+    # check that the required attribute is present for a requested operation
+    def check_for_these_attributes(self, attributes: list = []):
+        current = self.inst_attributes()
+        not_current = [x for x in attributes if x not in current.keys()]
+        if len(not_current):
+            data = False
+        else:
+            data = True
+        return data, not_current
 
+    # check for data type prior to attempting operations
+    def check_instance_of(self, obj: object = None, isof: type = None):
+        """Check the type of data being passed"""
+        return isinstance(obj, isof)
 
+    def make_fd(self, sources: list = []):
+        """Applies the user defined variables to construct the feature data
+
+        Make_fd is the base method for this class. It assigns a unique id for
+        each survey, slices the data by start and end dates and applies
+        self.level and self.loi to slice the data by hierarchical types.
+
+        Provides two sets of data for each source. One set has all the
+        data within the date range, the other is a slice with just the data
+        from the loi attribute.
+
+        :param sources: A list that has the attribute name for the data source
+        :type sources: list
+        :return: None: results are added as attributes to the class
+        instance
+        """
+
+        # check for the required attributes
+        # source of data, locations of interest
+        status, labels = self.check_for_these_attributes([*self.min_att,
+                                                          *sources])
+
+        if status:
+            # if all the attributes are present
+            # make the feature data, add as class attribute
+            for x in sources:
+                # the location of the file is an attribute of the class
+                location = getattr(self, x)
+                # get the data
+                a = pd.read_csv(location)
+                # format the unique survey id
+                a["loc_date"] = list(zip(a["location"], a["date"]))
+                # set to datetime
+                a["date"] = pd.to_datetime(a["date"])
+                # slice by start and end
+                aslice = (a["date"] >= self.sdate) & (a["date"] <= self.edate)
+                ad = a[aslice].copy()
+
+                # assign all the data as an attribute
+                new_name = f"a_data_{x}"
+                setattr(self, new_name, ad)
+
+                # slice the data by the location attributes
+                fd = feature_data(ad, self.level, loi=self.loi)
+                # assign the feature data as an attribute
+                setattr(self, x, fd)
+
+        else:
+            # if not identify the missing attributes
+            # print to console
+            print("The following attributes are undefined", labels)
+
+    def change_column_names(self, attname: str = "fd", renames: dict =
+                            column_rename):
+        """User defined column names are added here"""
+        status, labels = self.check_for_these_attributes([attname])
+        if status:
+            data = getattr(self, attname)
+            if isinstance(data, pd.core.frame.DataFrame):
+                data.rename(columns=renames, inplace=True)
+                setattr(self, attname, data)
+            else:
+                print("that attribute is not a dataframe")
+        else:
+            print("there is no attribute with that name")
+
+    def summary(self, attname: str = "fd"):
+        """The summary of samples, cities and population in the data.
+
+        If the columns are present in the fd attribute: summarizes the
+        number of locations, samples, cities, population and quantity for
+        the provided data.
+
+        :param attname: An instance attribute that is a dataframe
+        :type attname: str
+        :return: None: results are added as attributes to the class
+        instance
+        """
+        # the required columns
+        required = ["population", "loc_date",
+                    self.component, "location",
+                    "quantity"
+                    ]
+
+        # retrieve the attribute that has the data
+        try:
+            somdata = getattr(self, attname)
+
+            # if somdata resolves to a data frame
+            if self.check_instance_of(somdata, pd.core.frame.DataFrame):
+                cols = [x for x in required if x in somdata.columns]
+                # this holds the summary values if len(cols) > 0
+                # gets added as an attribute of the class instance
+                asummary = dict()
+
+                if len(cols):
+                    # provide a summary of the different column values
+                    for acol in cols:
+                        if acol in ["loc_date", self.component, "location"]:
+                            asummary.update({acol: somdata[acol].nunique()})
+                        if acol in [self.component, "location"]:
+                            asummary.update({
+                                f"{acol}_list": somdata[acol].unique()
+                            })
+                        if acol == "quantity":
+                            asummary.update({acol: somdata.quantity.sum()})
+                        if acol == "population":
+                            popd = somdata[
+                                ["city", "population"]
+                            ].drop_duplicates()
+                            population = popd.population.sum()
+                            asummary.update({acol: population})
+
+                    # set the asummary dict as an attribute of the instance
+                    setattr(self, f"{attname}_summary", asummary)
+                else:
+                    # the summary cannot be completed
+                    print("The required columns are not in the dataframe")
+            else:
+                # either use a different attribute or load the data
+                print("That attribute does not resolve to a dataframe")
+        except:
+            print("That is not a valid attribute for this instance")
+
+    def code_totals(self, attname: str = "fd"):
+        """ The cumulative values of the codes for the requested data.
+
+        The total number of each object code identified. The % of the total
+        numer of objects, the fail rate and the median unit_label value is
+        also calculated. The results are added as attributes to the class
+        instance.
+
+        :param attname: An instance attribute that is a dataframe
+        :type attname: str
+        :return: None: results are added as attributes to the class
+        instance
+        """
+
+        try:
+            somdata = getattr(self, attname)
+
+            if self.check_instance_of(somdata, pd.core.frame.DataFrame):
+                required = ["loc_date", "code", "quantity", self.unit_label]
+                cols = [x for x in required if x in somdata.columns]
+                if len(cols) == len(required):
+                    # per sample numbers
+                    g_one = ["loc_date", "code"]
+                    agg_this = {self.unit_label: "sum", "quantity": "sum"}
+                    a = somdata.groupby(g_one, as_index=False).agg(agg_this)
+
+                    # count the number of times none were found
+                    a["fail"] = a.quantity > 0
+                    aggthat = {"fail": "sum",
+                               "quantity": "sum",
+                               self.unit_label: "median"}
+
+                    b = a.groupby("code").agg(aggthat)
+                    b["% of total"] = b.quantity/b.quantity.sum()
+                    b["fail rate"] = b.fail/somdata.loc_date.nunique()
+
+                    setattr(self, f"{attname}_codetotals", b)
+                else:
+                    print("The required columns are not in the dataframe")
+            else:
+                # either use a different attribute or load the data
+                print("That attribute does not resolve to a dataframe")
+        except:
+            print("That is not a valid attribute for this instance")
+
+    def survey_summary(self, attname: str = "fd"):
+        """Aggregates the total for each survey and summarizes the results.
+
+        Survey summary aggregates the unit_label variable to the unique
+        survey id. Producing the survey total for each survey. The summary
+        is the default of pands.series.describe. The results (the time
+        series data of survey results and the summary) are
+        added as attributes to the class instance.
+
+        :param attname: An instance attribute that is a dataframe
+        :type attname: str
+        :return: None: results are added as attributes to the class
+        instance
+        """
+        try:
+            somdata = getattr(self, attname)
+            if self.check_instance_of(somdata, pd.core.frame.DataFrame):
+                required = ["loc_date", "date", self.unit_label]
+                cols = [x for x in required if x in somdata.columns]
+                if len(cols) == len(required):
+                    sids = ["loc_date", "date"]
+                    a = somdata.groupby(sids, as_index=False)[
+                        self.unit_label
+                        ].sum()
+                    setattr(self, f"{attname}_surveytotals", a)
+
+                    asummary = a[self.unit_label].describe()
+                    setattr(self, f"{attname}_surveysummary", asummary)
+
+                else:
+                    print("The required columns are not in the dataframe")
+            else:
+                print("That attribute does not resolve to a dataframe")
+        except:
+            print("That is not a valid attribute for this instance")
+
+    def component_summary(self, attname: str = 'fd'):
+        """Aggregates survey results of the components
+
+        Provides the number of samples, the total number of objects and the
+        median unit_label value for each of the components.
+
+        :param attname: An instance attribute that is a dataframe
+        :type attname: str
+        :return: None: results are added as attributes to the class
+        instance
+        """
+        somdata = getattr(self, attname)
+
+        if self.check_instance_of(somdata, pd.core.frame.DataFrame):
+            required = ["loc_date", "date",
+                        self.unit_label, self.component,
+                        "quantity"]
+            cols = [x for x in required if x in somdata.columns]
+
+            if len(cols) == len(required):
+                sids = [self.component, "loc_date"]
+                agg_this = {self.unit_label: "sum", "quantity": "sum"}
+                a = somdata.groupby(sids, as_index=False).agg(agg_this)
+                asum = {self.unit_label: "median", "quantity": "sum",
+                        "loc_date": "nunique"}
+                b = a.groupby(self.component, as_index=False).agg(asum)
+                setattr(self, f"{attname}_components", b)
+
+            else:
+                print("The required columns are not in the dataframe")
+        else:
+            print("That attribute does not resolve to a dataframe")
 
 
